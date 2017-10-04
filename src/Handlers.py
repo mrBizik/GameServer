b80739d83f3acd6a91963d631d831fa0e30d5691
mrBizik <bizigg@gmail.com>
2017-10-04 22:04:18 +0500

    JSONRpc module

--- src/Handlers.py
+++ src/Handlers.py
@@ -1,76 +1,47 @@
-import tornado.ioloop as ioloop
 import tornado.web as web
 
 from src.core import Commands
-from src.server import RPCWSocket as Socket
+from src.server.JSONRpc import RpcWebSocket, RpcHandler
 
 
 class IndexHandler(web.RequestHandler):
     def get(self, *args, **kwargs):
-        self.render('templates/index.html', title="ECS engine", canvas={
+        self.render('server/templates/index.html', title="ECS engine", canvas={
             "id": "test-canvas",
             "width": 700,
             "height": 700
         })
 
 
-class GameHandler(web.RequestHandler):
-    def post(self, *args, **kwargs):
-        action = self.get_argument('action')
-        params = None
-        result = None
-        self._register_user()
-        if self.get_argument('args'):
-            params = json.loads(self.get_argument('args'))
-
-        if action == 'new':
-            result = {'state': self._new_game()}
-
-        if action == 'connect':
-            result = {'result': self._connect_to_game(params['idGame'])}
-
-        self.render('templates/answer.html', result=result)
-
-    def _connect_to_game(self, id_game):
+class GameHandler(RpcHandler):
+    def rpc_connect_to_game(self, params):
         game_config = None
+        id_game = params['idGame']
         game = self.application.game_pool.connect_to_game(id_game)
         if game:
             game_config = game.get_config('frontend')
         return game_config
 
-    def _new_game(self):
-        game_config = None
-        game = self.application.game_pool.new_game()
-        if game:
-            ioloop.IOLoop.instance().add_callback(game.game_loop)
-            self._connect_to_game(game.id)
-            # self.set_cookie('game_id', str(game.id))
-            game_config = game.get_config('frontend')
-        return game_config
-
-    def _register_user(self):
+    def register_user(self):
         if not self.get_cookie('user_id'):
-            self.set_cookie('user_id', str(self.application.next_user_id.next()))
+            self.set_cookie('user_id', '1')
 
 
-class GameSocket(Socket.RPCWSocket):
+class GameSocket(RpcWebSocket):
     def __init__(self, application, request, **kwargs):
         super(GameSocket, self).__init__(application, request, **kwargs)
         # self.user_id = self.get_cookie('user_id')
         # TODO: hardcode
         self.user_id = 1
         self.game = self.application.game_pool.get_game(0)
-        self.game.add_game_listener(self._on_game_update)
+        self.game.add_game_listener(self.on_game_update)
 
-    def _on_game_update(self, token_list):
+    def on_game_update(self, token_list):
         # TODO: временный костыль пока нормальное завершение игры не сделаю
         if self.ws_connection is not None:
             self.write_message(token_list.get())
 
-    def _close_socket(self):
-        print('close_socket')
-
-    def move(self, params):
+    def rpc_move(self, params):
         # TODO: Не факт что сущность равна id_user
         # params['id'] = self.user_id
         params['id'] = 0
---
+++ src/server/JSONRpc.py
@@ -0,0 +1,159 @@
+import json
+
+from tornado.websocket import WebSocketHandler
+from tornado.web import RequestHandler
+
+import uuid
+
+JSON_PARSE_ERR = {
+    "code": -1,
+    "message": "JSON parse error"
+}
+
+METHOD_NOT_FOUND = {
+    "code": -32601,
+    "message": "Method not found"
+}
+
+
+# TODO: make batch
+# TODO: make async answer
+
+
+class Rpc:
+    @staticmethod
+    def rpc_method_prefix():
+        return 'rpc_'
+    """
+        Метод предназначен для определения списка процедур,
+        которые можно вызвать в конкретном хэндлере.
+        Методы содержащие в имени _(землю) считаются приватными и отбрасываются
+    """
+    @staticmethod
+    def init_rpc_methods(context):
+        result = []
+        all_methods = list(context.__class__.__dict__)
+        for i in all_methods:
+            if i.startswith(Rpc.rpc_method_prefix()):
+                result.append(i)
+        return result
+
+    """
+        Разобрать json полученный от клиента
+    """
+    @staticmethod
+    def parse_message(message):
+        parsed = json.loads(message)
+        return parsed["method"], parsed["params"], parsed["id"]
+
+    """
+        Распарсить параметры в соответствии с ключами в param_keys
+    """
+    @staticmethod
+    def parse_params(param_keys, raw_params):
+        result = {}
+        i = 0
+        # TODO: проверка длинны raw_param, установка дефолтных значений
+        for key in param_keys:
+            result[key] = raw_params[i]
+            i += 1
+
+        return result
+
+    @staticmethod
+    def make_message(result=None):
+        error = None
+        message = None
+        try:
+            if result:
+                message = json.dumps({
+                    "jsonrpc": "2.0",
+                    "id": str(uuid.uuid1()),
+                    "result": result
+                })
+            else:
+                message = json.dumps({
+                    "jsonrpc": "2.0",
+                    "id": str(uuid.uuid1())
+                })
+        except Exception:
+            error = Rpc.make_error(JSON_PARSE_ERR)
+        finally:
+            return message, error
+
+    @staticmethod
+    def make_error(error):
+        message = {
+            "jsonrpc": "2.0",
+            "id": str(uuid.uuid1()),
+            "error": {
+                "code": error["code"],
+                "message": error["message"]
+            }
+        }
+
+        return json.dumps(message)
+
+    @staticmethod
+    def call_method(context, method, params,):
+        error = None
+        message = None
+        method = Rpc.rpc_method_prefix() + method
+        try:
+            context.rpc_methods.index(method)
+            result = context.__class__.__dict__[method](context, params)
+            message, error = Rpc.make_message(result)
+        # Метод не найден
+        except ValueError:
+            # TODO add to log message
+            error = Rpc.make_error(METHOD_NOT_FOUND)
+        finally:
+            return message, error
+
+
+class RpcWebSocket(WebSocketHandler):
+    def __init__(self, application, request, **kwargs):
+        self.rpc_methods = Rpc.init_rpc_methods(self)
+        super(RpcWebSocket, self).__init__(application, request, **kwargs)
+
+    def open(self, *args, **kwargs):
+        result, error = Rpc.make_message()
+        if error:
+            result = error
+        self.write_message(result)
+
+    def on_message(self, message):
+        method, params, id = Rpc.parse_message(message)
+        message, error = Rpc.call_method(self, method, params)
+        self.write_message(message, error)
+
+    def send_message(self, result, error=None):
+        if not error:
+            message, error = Rpc.make_message(result)
+        else:
+            message = error
+        self.write_message(message)
+
+
+class RpcHandler(RequestHandler):
+    def __init__(self, application, request, **kwargs):
+        self.rpc_methods = Rpc.init_rpc_methods(self)
+        self.template = 'templates/answer.html'
+        super(RpcHandler, self).__init__(application, request, **kwargs)
+
+    def post(self, *args, **kwargs):
+        self.add_header('content_type', 'application/json-rpc')
+        method = self.get_argument('method')
+        params = json.loads(self.get_argument('params'))
+        result, error = Rpc.call_method(self, method, params)
+        if error:
+            result = error
+        self.render(self.template, result=result)
+
+    def get(self, *args, **kwargs):
+        self.add_header('content_type', 'application/json-rpc')
+        method, params, id = Rpc.parse_message(self.get_argument('r'))
+        result, error = Rpc.call_method(self, method, params)
+        if error:
+            result = error
+        self.render(self.template, result=result)
